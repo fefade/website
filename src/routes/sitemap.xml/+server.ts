adb55dba01fd5fa79c22f6dfcfbbfc380605fff5
Diógenes Rodrigues <37380674+dxdns@users.noreply.github.com>
2026-02-24 12:37:31 -0300

    add: sitemap

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,30 @@
+export async function GET() {
+	const urls = ["/", "/cookie-policy", "/privacy-policy", "/terms-of-use"]
+		.map((route) => {
+			return `
+		<url>
+			<loc>https://fefade.com${route}</loc>
+		</url>`.trim()
+		})
+		.join("\n")
+
+	return new Response(
+		`
+		<?xml version="1.0" encoding="UTF-8" ?>
+		<urlset
+			xmlns="https://www.sitemaps.org/schemas/sitemap/0.9"
+			xmlns:xhtml="https://www.w3.org/1999/xhtml"
+			xmlns:mobile="https://www.google.com/schemas/sitemap-mobile/1.0"
+			xmlns:news="https://www.google.com/schemas/sitemap-news/0.9"
+			xmlns:image="https://www.google.com/schemas/sitemap-image/1.1"
+			xmlns:video="https://www.google.com/schemas/sitemap-video/1.1"
+		>
+            ${urls}
+		</urlset>`.trim(),
+		{
+			headers: {
+				"Content-Type": "application/xml"
+			}
+		}
+	)
+}
