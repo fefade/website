91a633e38eec24efd234aae3f229a5e10cab0bd6
Diógenes Rodrigues <37380674+diorodrigu@users.noreply.github.com>
2026-04-09 14:31:42 -0300

    chore: language strategy

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,14 @@ export default defineConfig({
 		paraglideVitePlugin({
 			project: "./project.inlang",
 			outdir: "./src/lib/paraglide",
-			strategy: ["url", "cookie", "baseLocale"]
+			strategy: [
+				"preferredLanguage",
+				"baseLocale",
+				"url",
+				"cookie",
+				"localStorage",
+				"globalVariable"
+			]
 		})
 	]
 })
