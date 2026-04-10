f87321e77ee71d525d45e451a7376675eca228cf
Diógenes Rodrigues <37380674+diorodrigu@users.noreply.github.com>
2026-04-10 09:14:28 -0300

    chore: normalize lang

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,12 +1,18 @@
 import type { Handle } from "@sveltejs/kit"
 import { paraglideMiddleware } from "$lib/paraglide/server"
 
+function normalizeLang(lang: string) {
+	const [l, r] = lang.split("-")
+	return r ? `${l.toLowerCase()}-${r.toUpperCase()}` : l.toLowerCase()
+}
+
 const handleParaglide: Handle = ({ event, resolve }) =>
 	paraglideMiddleware(event.request, ({ request, locale }) => {
 		event.request = request
 
 		return resolve(event, {
-			transformPageChunk: ({ html }) => html.replace("%paraglide.lang%", locale)
+			transformPageChunk: ({ html }) =>
+				html.replace("%paraglide.lang%", normalizeLang(locale))
 		})
 	})
 
